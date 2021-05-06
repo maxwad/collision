72406d178d6871de877fcc23db58fa5a59d85a06
maxwad <maxwad@yandex.ru>
2021-05-06 20:13:45 +0300

    Gizmos

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,7 +7,7 @@ public class Movement : MonoBehaviour
     public Transform player;
     public Transform ground;
 
-    public float mass = 1;    
+    public float mass;    
 
     // other Force and direction
     private float force = 3f;
@@ -26,12 +26,11 @@ public class Movement : MonoBehaviour
 
     private Boolean IsGrounded()
     {
-        bool isGrounded = false;
-                
+        bool isGrounded = false;       
         float dX = cosGroundAngle * (ground.localScale.x / 2);
         float dY = sinGroundAngle * (ground.localScale.x / 2);
         Vector2 distance = new Vector2(dX, dY) + (Vector2)ground.position - (Vector2)player.position;
-        Vector2 projectionDistance = Vector2.Dot(distance, normal) * normal;
+        Vector2 projectionDistance = Vector2.Dot(distance, normal) * normal;     
 
         if (projectionDistance.magnitude - 0.1 <= player.localScale.y / 2)
         {            
@@ -40,10 +39,22 @@ public class Movement : MonoBehaviour
         return isGrounded;
     }
 
+
+    private void DrawNormal()
+    {
+        Vector2 deltaVector     = new Vector2(cosGroundAngle * (ground.localScale.x), sinGroundAngle * (ground.localScale.x)) /2;
+        Vector2 groundLevel     = (Vector2)ground.position - deltaVector;
+        Vector2 poitOfCollision = new Vector2(player.position.x - groundLevel.x, (player.position.x - groundLevel.x) * (sinGroundAngle / cosGroundAngle));
+        Vector2 giz             = new Vector2 (player.position.x, groundLevel.y + poitOfCollision.y);
+
+        Debug.DrawLine(giz, giz - normal, Color.green);        
+    }
+
+
+
     void Start()
     {
-        float gravity = 10;
-        float angleGravityRad = -90 * (float)Math.PI / 180;
+        Vector2 gravity = new Vector2(0, -10);
 
         groundAngle    = ground.transform.rotation.eulerAngles.z * (float)Math.PI / 180;
         cosGroundAngle = (float)Math.Cos(groundAngle);
@@ -55,17 +66,19 @@ public class Movement : MonoBehaviour
         // Components before start
         acceleration = new Vector2(
             (force / mass) * (float)Math.Cos(directionForceRad), 
-            gravity * (float)Math.Sin(angleGravityRad) + (force / mass) * (float)Math.Sin(directionForceRad)
-            );
+            (force / mass) * (float)Math.Sin(directionForceRad) + gravity.y
+            );        
     }
 
 
     void FixedUpdate()
     {       
-        // speed and distance (divide by 5 for fractionality)
+        // speed and distance
         movementSpeed   += acceleration * Time.deltaTime;
         delta           = movementSpeed * Time.deltaTime;        
         player.position = (Vector2)player.position + delta;
+
+        DrawNormal();
 
         // check collision and change direction
         if ((player.position.x > (leftEdge - player.localScale.x / 2)) && 
